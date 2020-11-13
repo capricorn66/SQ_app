34d577d7ba130d2267c7070e05dc016cd13a3296
Michal <michterlecki@gmail.com>
2020-11-13 20:43:42 +0100

    v2.9

--- app/js/script.js
+++ app/js/script.js
@@ -4,12 +4,8 @@ import "./hasAttr";
 import {rwdMedia} from "./rwdMedia";
 import {rippletInit} from './ripplet';
 import './nav-scroll';
-import 'bootstrap/js/dist/modal';
-import 'bootstrap/js/dist/popover';
-import 'bootstrap/js/dist/alert';
-import 'bootstrap/js/dist/toast';
+import 'bootstrap';
 import 'jquery-ui-sortable-npm';
-
 
 window.debounce = debounce;
 window.rwdMedia = rwdMedia;
